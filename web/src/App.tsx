1c13fd90a1949cf4d031e3f166039b83c1cd3493
rafamedeiros0 <rafamedeiros0@gmail.com>
2022-09-13 20:18:10 -0300

    initial config tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,20 +1,8 @@
-interface ButtonProps {
-  title: string;
-}
+import './styles/main.css'
 
-function Button(props: ButtonProps) {
-  return (
-    <button>
-      {props.title}
-    </button>
-  )
-}
 function App() {
   return (
     <div>
-      <Button title="Send 1" />
-      <Button title="Send 2" />
-      <Button title="Send 3" />
     </div>
   )
 }
