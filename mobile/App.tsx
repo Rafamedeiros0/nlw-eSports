d616393d50045469f3e8911884f752db3f618868
rafamedeiros0 <rafamedeiros0@gmail.com>
2022-09-13 21:46:02 -0300

    config standard color and clean app.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,39 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-interface ButtonProps {
-  title: string
-}
-
-function Button( props: ButtonProps ) {
-  return (
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
 
-      <Button title='Send 1'></Button>
-      <Button title='Send 2'></Button>
-      <Button title='Send 3'></Button>
-      <Button title='Hello World'></Button>
-
-      <StatusBar style='auto' />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
