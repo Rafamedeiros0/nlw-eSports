f9b4b91733b90e8b801d8e745a943d837587cee5
rafamedeiros0 <rafamedeiros0@gmail.com>
2022-09-14 21:33:08 -0300

    create all routes for the use cases

---
+++ server/README.md
@@ -0,0 +1,24 @@
+# Back-end
+
+## Entidades
+
+### Game
+id
+tittle
+bannerUrl
+### Ad
+id
+gameID
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+## Casos de uso
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anúncios por game
+- Buscar discord pelo ID do anúncio
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,36 @@ import express from "express"
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+// HTTP methods / API RESTful / HTTP Codes
+
+/**
+ * Query: localhost:3333/ads?page=2 (persistencia e para coisas não sensiveis)
+ * Route: localhost:3333/ads/5  (acessar o anuncio com identificador 5)
+ * Body: quando vamos enviar varias informacoes em uma unica requisição (fica escondido na requisição)
+ */
+
+app.get('/games', (request, response) => {
+  return response.json([])
+})
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([])
+})
+
+app.get('/games/:id/ads', (request, response) => {
+  const gameId = request.params.id
+
   return response.json([
     { id: 1, name: 'Anuncio 1'},
     { id: 2, name: 'Anuncio 2'},
     { id: 3, name: 'Anuncio 3'}
   ])
+})
+
+app.get('/ads/:id/discord', (request, response) => {
+  const adId = request.params.id
+  
+  return response.json([])
 })
 
 app.listen(3333)
