07d4402a8c0e8934a8c6ae6e46575e1346e9007f
rafamedeiros0 <rafamedeiros0@gmail.com>
2022-09-14 22:36:03 -0300

    config the route about list ads

--- server/src/server.ts
+++ server/src/server.ts
@@ -25,14 +25,35 @@ app.post('/ads', (request, response) => {
   return response.status(201).json([])
 })
 
-app.get('/games/:id/ads', (request, response) => {
+app.get('/games/:id/ads', async (request, response) => {
   const gameId = request.params.id
 
-  return response.json([
-    { id: 1, name: 'Anuncio 1'},
-    { id: 2, name: 'Anuncio 2'},
-    { id: 3, name: 'Anuncio 3'}
-  ])
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hourStart: true,
+      hourEnd: true,
+    },
+    where: {
+      gameId,
+    },
+    orderBy: {
+      createdAt: 'desc'
+    }
+  })
+
+
+
+  return response.json(ads.map(ad => {
+    return {
+      ...ad,
+      weekDays: ad.weekDays.split(',')
+    }
+  }))
 })
 
 app.get('/ads/:id/discord', (request, response) => {
